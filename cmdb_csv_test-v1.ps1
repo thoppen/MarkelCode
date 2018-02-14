8f7241cbc42bbd9824142d98a362dbbfa35266c0
Hoppenjans <terry.hoppenjans@emc.com>
2018-02-14 13:35:43 -0600

    Update with Markel fields.

--- cmdb_csv_test-v1.ps1
+++ cmdb_csv_test-v1.ps1
@@ -1,6 +1,29 @@
 ﻿Param(
-    [Parameter(Mandatory=$False)][String]$Type = "test",               #  Server Type - Server/Workstation
-    [Parameter(Mandatory=$False)][Int]$Memory = 1                #  Total Memory (in GB)
+    [Parameter(Mandatory=$True)][String]$Type,               # Type
+    [Parameter(Mandatory=$True)][String]$ciStatus,           # CI Status 
+    [Parameter(Mandatory=$True)][String]$assetTag,           # Asset Tag 
+    [Parameter(Mandatory=$True)][String]$manufacturer,       # Manufacturer 
+    [Parameter(Mandatory=$True)][String]$model,              # Model 
+    [Parameter(Mandatory=$True)][String]$serialNumber,       # Serial Number 
+    [Parameter(Mandatory=$True)][String]$networkIdentifier,  # Network Identifier
+    [Parameter(Mandatory=$True)][String]$responsiblePerson,  # Responsible Person
+    [Parameter(Mandatory=$True)][String]$mainUser,           # Main User
+    [Parameter(Mandatory=$True)][String]$deploymentType,     # Deployment Type
+    [Parameter(Mandatory=$True)][String]$systemType,         # System Type
+    [Parameter(Mandatory=$True)][Int]$numberOfCores,      # Number of Cores 
+    [Parameter(Mandatory=$True)][Int]$numberOfCpus,       # Number of CPUs
+    [Parameter(Mandatory=$True)][String]$location,           # Location 
+    [Parameter(Mandatory=$True)][String]$roleInCharge,       # Role In Charge
+    [Parameter(Mandatory=$True)][String]$functionalDomain,   # Functional Domain
+    [Parameter(Mandatory=$True)][String]$ipAddress,          # IP Address
+    [Parameter(Mandatory=$True)][Int]$totalCdisk,         # Total C Disk (gb) 
+    [Parameter(Mandatory=$True)][Int]$totalRam,           # Total RAM (GB) 
+    [Parameter(Mandatory=$True)][String]$itGroup,            # IT Group 
+    [Parameter(Mandatory=$True)][String]$itSubGroup,         # IT Sub Group
+    [Parameter(Mandatory=$True)][String]$nsxZone,            # NSX-Zone
+    [Parameter(Mandatory=$True)][Int]$totalDisk,          # Total Disk
+    [Parameter(Mandatory=$True)][String]$serverFunction,     # Server Function 
+    [Parameter(Mandatory=$True)][String]$hostedBusinessApp   # Hosted Business Application
 )
 
 Write-Host "Type: $Type"
@@ -14,8 +37,31 @@ Write-Host "Output File:  $FilePath"
 
 #  Create the output object record for the CSV export
 $Output = [PSCustomObject][Ordered]@{
-    'Type'                        = $Type
-    'Total RAM (GB)'              = $Memory
+    'Type'                          =  $Type             
+	'CI Status'                     =  $ciStatus         
+    'Asset Tag'                     =  $assetTag         
+    'Manufacturer'                  =  $manufacturer     
+    'Model'                         =  $model            
+    'Serial Number'                 =  $serialNumber     
+    'Network Identifier'            =  $networkIdentifier
+    'Responsible Person'            =  $responsiblePerson
+    'Main User'                     =  $mainUser         
+    'Deployment Type'               =  $deploymentType   
+    'System Type'                   =  $systemType       
+    'Number of Cores'               =  $numberOfCores    
+    'Number of CPUs'                =  $numberOfCpus     
+    'Location'                      =  $location         
+    'Role In Charge'                =  $roleInCharge     
+    'Functional Domain'             =  $functionalDomain 
+    'IP Address'                    =  $ipAddress        
+    'Total C Disk (gb)'             =  $totalCdisk       
+    'Total RAM (GB)'                =  $totalRam         
+    'IT Group'                      =  $itGroup          
+    'IT Sub Group'                  =  $itSubGroup       
+    'NSX-Zone'                      =  $nsxZone          
+    'Total Disk'                    =  $totalDisk        
+    'Server Function'               =  $serverFunction   
+    'Hosted Business Application'   =  $hostedBusinessApp
 }
 
 Write-Host $Output
